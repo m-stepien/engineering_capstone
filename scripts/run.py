27a77a91301db3b8410e5c6d09871bf6b8ebc91a
m-stepien <mstepien7@protonmail.com>
2024-11-10 23:23:04 +0100

    add script

---
+++ scripts/run.py
@@ -0,0 +1,13 @@
+import subprocess
+import os
+import sys
+
+nodes = ["engine_subscriber.py", "engine_data_handler.py", "servo_subscriber.py", "servo_data_handler.py", "main_publisher.py"]
+path = "./../src/"
+processes = []
+for node in nodes:
+    process = subprocess.Popen(["python3", path+node])
+    processes.append(process)
+
+for process in processes:
+    process.wait()
