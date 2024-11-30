92e484be4af0f0bdc8e580ad2a795ba7028ea549
m-stepien <mstepien7@protnmail.com>
2024-11-30 14:39:39 +0100

    mjpg test

--- src/came_mock.py
+++ src/came_mock.py
@@ -2,26 +2,28 @@ import cv2
 import socket
 import pickle
 import struct
+import os
+import glob
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host_ip = '0.0.0.0'  
 port = 12346
 server_socket.bind((host_ip, port))
 print(f"Init succesuful")
-
+mock_images_path = "frames/*.jpg"  
+mock_images = [cv2.imread(img_path) for img_path in sorted(glob.glob(mock_images_path))]
 cap = cv2.VideoCapture(0, cv2.CAP_V4L2)  
-if not cap.isOpened():
-    print("Camera not found!")
-    exit()
 
 try:
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
-        data = pickle.dumps(buffer)
-        server_socket.sendto(data, ('192.168.0.154', port))
+        for frame in mock_images:
+            if frame is None:
+                print("Failed to load mock frame.")
+                continue
+            encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
+            data = pickle.dumps(buffer)
+            server_socket.sendto(data, ('192.168.0.154', port))
+
 except Exception as e:
     print(f"Error: {e}")
 finally:
