ccdff73b47796a46bb51983a6ad86255ee73b373
m-stepien <mstepien7@protnmail.com>
2024-11-30 14:40:48 +0100

    run corr"

--- client_udp.py
+++ client_udp.py
@@ -10,21 +10,19 @@ client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 1)
 host_ip = 'inz.local'
 port = 12346
 
-client_socket.bind(('', port))  # Bind to any IP for receiving
+client_socket.bind(('', port)) 
 print(f"Client ready to receive video from {host_ip}:{port}")
 
 try:
     while True:
-        # Receive data
-        packet, _ = client_socket.recvfrom(65535)  # Max UDP packet size
+        packet, _ = client_socket.recvfrom(65535) 
         data = pickle.loads(packet)
 
-        # Decode the frame
+
         frame = cv2.imdecode(np.frombuffer(data, np.uint8), cv2.IMREAD_COLOR)
 
-        # Display the frame
         cv2.imshow('Client: Video Stream', frame)
-        if cv2.waitKey(1) == 27:  # Press Esc to exit
+        if cv2.waitKey(1) == 27:  
             break
 except Exception as e:
     print(f"Error: {e}")
--- src/scripts/run.py
+++ src/scripts/run.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import sys
 
-nodes = ["engine_subscriber.py", "engine_data_handler.py", "servo_subscriber.py", "servo_data_handler.py", "main_publisher.py", "camera.py"]
+nodes = ["engine_subscriber.py", "engine_data_handler.py", "servo_subscriber.py", "servo_data_handler.py", "main_publisher.py", "camera.py", "came.py"]
 path = "./../"
 processes = []
 for node in nodes:
