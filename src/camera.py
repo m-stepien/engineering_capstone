0a989bbe9d5cac2b51d3a8c77e905d13c181cc41
Mateusz Stępień <mateusz.stepien@student.up.krakow.pl>
2024-12-13 13:58:03 +0100

    przerobienie camery na klase

--- src/camera.py
+++ src/camera.py
@@ -3,43 +3,52 @@ import socket
 import pickle
 import struct
 import time
+import paho.mqtt.client as mqtt
 
+class Camera():
+    def __init__(self, broker_address="localhost", frame_to_send_number=40):
+        self.client = mqtt.Client("Camera")
+        self.frame_counter = 0
+        self.frame_to_send_number = frame_to_send_number
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.host_ip = '0.0.0.0'  
+        self.port = 12346
+        self.server_socket.bind((self.host_ip, self.port))
+        self.client.connect(broker_address)
+        self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)  
+        self.fps = 30
+        self.cap.set(cv2.CAP_PROP_FPS, fps)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320) #640
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240) #480
+        self.cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+        self.frame_duration = 1 / self.fps 
+        if not self.cap.isOpened():
+            print("Error: Camera not initialized!")
+            self.cap.release()
+            self.server_socket.close()
+            exit()
+        print(f"Init succesuful")
 
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-host_ip = '0.0.0.0'  
-port = 12346
-server_socket.bind((host_ip, port))
-print(f"Init succesuful")
-
-cap = cv2.VideoCapture(0, cv2.CAP_V4L2)  
-fps = 30
-cap.set(cv2.CAP_PROP_FPS, fps)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320) #640
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240) #480
-cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-frame_duration = 1 / fps 
-
-if not cap.isOpened():
-    print("Error: Camera not initialized!")
-    cap.release()
-    server_socket.close()
-    exit()
-try:
-    last_frame_time = time.perf_counter()
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        encoded, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
-        server_socket.sendto(buffer, ('192.168.0.124', port))
-        elapsed_time = time.perf_counter() - last_frame_time
-        if elapsed_time < frame_duration:
-            time.sleep(frame_duration - elapsed_time)
-        last_frame_time = time.perf_counter()
-except Exception as e:
-    print(f"Error: {e}")
-finally:
-    cap.release()
-    cv2.destroyAllWindows()
-    server_socket.close()
+    def start_camer(self):
+        try:
+            last_frame_time = time.perf_counter()
+            while True:
+                ret, frame = self.cap.read()
+                if not ret:
+                    break
+                _, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 50])
+                self.server_socket.sendto(buffer, ('192.168.0.124', self.port))
+                elapsed_time = time.perf_counter() - last_frame_time
+                if elapsed_time < self.frame_duration:
+                    time.sleep(self.frame_duration - elapsed_time)
+                self.frame_counter +=1
+                if self.frame_counter == self.frame_to_send_number:
+                    pass
+                last_frame_time = time.perf_counter()
+        
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            self.cap.release()
+            cv2.destroyAllWindows()
+            self.server_socket.close()
