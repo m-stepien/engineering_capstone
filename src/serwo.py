fdbd6834fbed46e2d5ee7163ce39d19328274899
Mateusz Stępień <mateusz.stepien@student.up.krakow.pl>
2024-12-14 03:37:11 +0100

    próba modyfikacji mechanizmu ruchu servo

--- src/serwo.py
+++ src/serwo.py
@@ -1,6 +1,7 @@
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import AngularServo
 from time import sleep
+import threading
 
 SERVO_CONTROL_PIN = 12
 MIN_PULSE_WIDTH = 0.4/1000
@@ -9,16 +10,29 @@ MAX_PULSE_WIDTH = 2.6/1000
 class Servo(AngularServo):
     def __init__(self):
         super().__init__(SERVO_CONTROL_PIN, min_pulse_width=MIN_PULSE_WIDTH, max_pulse_width=MAX_PULSE_WIDTH, pin_factory=PiGPIOFactory())
-        self.initial_position = 0
-        self.move(0)
+        self.position = 0
+        self.move(self.position)
+        self.go_to = 0
+        self.step = 1
+        self.pause = 0.05
+        self.current_work = None
+        self.running = False
 
     def move(self, angle):
-        self.angle=self.initial_position + angle
-        
-
-class ServoMock():
-    def __init__():
-        self.file_name = "serwo_angle.txt"
-        with open(self.file_name, "w+") as f:
-            f.write("Start\n")
+        while self.running:
+            if self.position < self.go_to:
+                self.position += self.step
+            elif self.position > self.go_to:
+                self.position -= self.step
+            sleep(self.pause)
     
+
+    def start_go_to(self, angle):
+        if self.current_work is None:
+            self.running = True
+            self.current_work = threading.Thread(target=self.move, daemon=True)
+            self.current_work.start()
+        self.go_to = angle
+
+
+#ewentualne dołożenie dynamicznej zmiany stepu
