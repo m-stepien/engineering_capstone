397cdc1aa2666a070436d47388c697b312d19031
Mateusz Stępień <mateusz.stepien@student.up.krakow.pl>
2024-12-14 04:18:14 +0100

    poprawki

--- src/serwo.py
+++ src/serwo.py
@@ -10,8 +10,7 @@ MAX_PULSE_WIDTH = 2.6/1000
 class Servo(AngularServo):
     def __init__(self):
         super().__init__(SERVO_CONTROL_PIN, min_pulse_width=MIN_PULSE_WIDTH, max_pulse_width=MAX_PULSE_WIDTH, pin_factory=PiGPIOFactory())
-        self.position = 0
-        self.move(self.position)
+        self.move(0)
         self.go_to = 0
         self.step = 1
         self.pause = 0.05
@@ -20,19 +19,25 @@ class Servo(AngularServo):
 
     def move(self, angle):
         while self.running:
-            if self.position < self.go_to:
-                self.position += self.step
-            elif self.position > self.go_to:
-                self.position -= self.step
+            if self.angle < self.go_to:
+                self.angle += self.step
+            elif self.angle > self.go_to:
+                self.angle -= self.step
             sleep(self.pause)
     
 
     def start_go_to(self, angle):
-        if self.current_work is None:
+        if not self.running:
             self.running = True
             self.current_work = threading.Thread(target=self.move, daemon=True)
             self.current_work.start()
         self.go_to = angle
+
+
+    def stop(self):
+        self.running = False
+        if self.current_work is not None:
+            self.current_work.join()
 
 
 #ewentualne dołożenie dynamicznej zmiany stepu
