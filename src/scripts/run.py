50b891621ba4432d4bdb7733973006c6fc24b265
Mateusz Stępień <mateusz.stepien@student.up.krakow.pl>
2024-12-21 22:58:18 +0100

    ... now test

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -3,8 +3,7 @@ import os
 import sys
 #delete mock after when writting frontend comunication of current speed end
 #"engine_subscriber_mock.py", "send_jpg_to_binary.py"
-# nodes = ["engine_subscriber.py", "engine_data_handler.py", "servo_subscriber.py", "servo_data_handler.py", "ai_service.py", "main_publisher.py", "camera.py"]
-nodes = ["main_publisher.py"]
+nodes = ["engine_subscriber.py", "engine_data_handler.py", "servo_subscriber.py", "servo_data_handler.py", "ai_service.py", "main_publisher.py", "camera.py"]
 
 path = ""
 processes = []
