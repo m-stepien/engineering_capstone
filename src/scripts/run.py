45c3e8941bb60e86f5037dba05f87480b812af22
Mateusz Stępień <mateusz.stepien@student.up.krakow.pl>
2024-12-22 00:28:53 +0100

    delete test run

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -1,8 +1,6 @@
 import subprocess
 import os
 import sys
-#delete mock after when writting frontend comunication of current speed end
-#"engine_subscriber_mock.py", "send_jpg_to_binary.py"
 nodes = ["engine_subscriber.py", "engine_data_handler.py", "servo_subscriber.py", "servo_data_handler.py", "ai_service.py", "main_publisher.py", "camera.py"]
 
 path = ""
