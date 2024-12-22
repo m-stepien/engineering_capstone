2dc85547def42009aec5b1e27a9fdbe99a0cb06b
Mateusz Stępień <mateusz.stepien@student.up.krakow.pl>
2024-12-22 15:36:57 +0100

    change dir to unrelative

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -1,9 +1,7 @@
 import subprocess
-import os
-import sys
 nodes = ["engine_subscriber.py", "engine_data_handler.py", "servo_subscriber.py", "servo_data_handler.py", "ai_service.py", "main_publisher.py", "camera.py"]
 
-path = ""
+path = "/capstone/src/"
 processes = []
 for node in nodes:
     process = subprocess.Popen(["python3", path+node])
