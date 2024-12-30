6e48520adef7683a8de6f229e7c9f10d7477c43c
Mateusz Stępień <mateusz.stepien@student.up.krakow.pl>
2024-12-30 23:30:37 +0100

    missing

--- car_ai/predict_yolo.py
+++ car_ai/predict_yolo.py
@@ -15,6 +15,6 @@ def get_detected_tag(image):
     max_area_index = areas.index(max(areas))
     largest_bbox_class = results.xywh[0][max_area_index, -1].item()
     largest_bbox_tag = detected_tags[int(largest_bbox_class)]
-
+    print(f"result {largest_bbox_tag}")
     return largest_bbox_tag or None
 
